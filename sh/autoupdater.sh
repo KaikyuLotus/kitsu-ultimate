0d2b50535f422cc527c3ac3c70565428a0271049
Kaikyu <kaikyu4k@gmail.com>
2019-11-27 23:54:12 +0100

    First release for autoupdater.sh

---
+++ sh/autoupdater.sh
@@ -0,0 +1,24 @@
+while true
+do
+  git fetch;
+  LOCAL=$(git rev-parse HEAD);
+  REMOTE=$(git rev-parse @{u});
+
+  # If our local revision id doesn't match the remote, we will need to pull the changes
+  if [ "{$LOCAL}" != "{$REMOTE}" ]; then
+    echo An update has been noticed
+
+    echo Killing Python
+    pkill python # Maybe we should kill only lotus.py...
+
+    # pull and merge changes
+    echo Pulling changes
+    git pull origin master;
+
+    echo Starting lotus.py
+    nohup python3.7 lotus.py &
+
+  fi
+  sleep 5
+  echo Checking again
+done
