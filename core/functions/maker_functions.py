6db7b7d3a75e89d06e730940073273dfcbf21b34
Kaikyu <kaikyu4k@gmail.com>
2020-04-07 00:05:51 +0200

    Fix for newbot

--- core/functions/maker_functions.py
+++ core/functions/maker_functions.py
@@ -8,9 +8,8 @@ def newbot(infos: Infos):
 
     bot_count = manager.get_user_bots_count(infos.user.uid)
 
-    if len(infos.message.args) == 1:
-        if infos.message.args[0] == "bypass":
-            bot_count = 0
+    if infos.user.is_maker_owner:
+        bot_count = 0
 
     if bot_count == 1:
         return infos.reply(f"You already have a bot..")
@@ -21,7 +20,7 @@ def newbot(infos: Infos):
         return infos.reply("{user.name} i think that this isn't a valid token...")
 
     infos.reply("Creating a new bot with this token...")
-    ok = mongo_interface.register_bot(infos.message.text, infos.user.uid)
+    ok = mongo_interface.register_bot(infos.message.args[0], infos.user.uid)
     if ok:
         infos.reply("Valid token! Your bot should be online now!")
     else:
